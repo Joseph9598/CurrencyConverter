d07232cc830abf3d22521eab5f926ff36742714d
Augusto <augustopereira280102@gmail.com>
2024-10-26 15:27:23 -0300

    add as funções do desafio,menu e concectando-se com a API

--- src/ConversorMoedas.java
+++ src/ConversorMoedas.java
@@ -7,52 +7,53 @@ public class ConversorMoedas {
 
     //aqui é o metodo principal onde vamos chamar todas as funções
     public static void main(String[] args) {
-        boolean continuar = true;
 
-        while (continuar) {
-            exibirMenu();
-            int opcao = lerOpcaoValida();
+        {
+            boolean continuar = true;
 
-            if (opcao == 7) {
-                continuar = false;
-                System.out.println("Programa finalizado!");
-            } else if (opcao >= 1 && opcao <= 6) {
-                String[] moedas = Moedas.obterMoedasParaConversao(opcao);
-                MetodoConverter.converterMoeda(moedas[0], moedas[1]);
-            } else {
-                System.out.println("Opção inválida, tente novamente...");
+            while (continuar) {
+                exibirMenu();
+                int opcao = lerOpcaoValida();
+
+                if (opcao == 7) {
+                    continuar = false;
+                    System.out.println("Programa finalizado!");
+                } else if (opcao >= 1 && opcao <= 6) {
+                    String[] moedas = Moedas.obterMoedasParaConversao(opcao);
+                    MetodoConverter.converterMoeda(moedas[0], moedas[1]);
+                } else {
+                    System.out.println("Opção inválida, tente novamente...");
+                }
             }
         }
-    }
 
 
-    private static void exibirMenu() {
+        private static void exibirMenu () {
 
-        System.out.println("Selecione uma opção de conversão:");
-        System.out.println("1 - USD para EUR");
-        System.out.println("2 - USD para GBP");
-        System.out.println("3 - USD para BRL");
-        System.out.println("4 - EUR para USD");
-        System.out.println("5 - GBP para USD");
-        System.out.println("6 - BRL para USD");
-        System.out.println("7 - Sair");
-    }
-    //Aqui valida-se a resposta do usuário quando vai selecionar a moeda de conversão ou sair do programa
-    private static int lerOpcaoValida() {
-        int opcao = scanner.nextInt();
-        return opcao;
-    }
-
-
-
-    // Função para ler e validar o valor digitado pelo usuário
-    public static double lerValorValido() {
-        while (!scanner.hasNextDouble()) {
-            System.out.println("Por favor, digite um valor numérico válido.");
-            scanner.next();  // Descarta a entrada inválida
+            System.out.println("Selecione uma opção de conversão:");
+            System.out.println("1 - USD para EUR");
+            System.out.println("2 - USD para GBP");
+            System.out.println("3 - USD para BRL");
+            System.out.println("4 - EUR para USD");
+            System.out.println("5 - GBP para USD");
+            System.out.println("6 - BRL para USD");
+            System.out.println("7 - Sair");
         }
-        return scanner.nextDouble();
+        //Aqui valida-se a resposta do usuário quando vai selecionar a moeda de conversão ou sair do programa
+        private static int lerOpcaoValida () {
+            int opcao = scanner.nextInt();
+            return opcao;
+        }
+
+
+        // Função para ler e validar o valor digitado pelo usuário
+        public static double lerValorValido () {
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Por favor, digite um valor numérico válido.");
+                scanner.next();  // Descarta a entrada inválida
+            }
+            return scanner.nextDouble();
+        }
+
     }
-
 }
-
